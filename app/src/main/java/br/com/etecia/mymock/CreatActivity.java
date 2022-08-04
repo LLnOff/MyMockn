a3fe219b6081622b8df07ab25b65a96f1d11e53d
lucas leonel <lucks9616@gmail.com>
2022-08-03 22:09:53 -0300

    Janela Start

---
+++ app/src/main/java/br/com/etecia/mymock/CreatActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.mymock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creat_layout);
+    }
+}
